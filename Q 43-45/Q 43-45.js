277de98e8d7928f57e4af43e16e4c3f79c21eb0f
tubaj@TubaJan <tubajan282@gmail.com>
2024-04-02 16:34:09 +0500

    45_Questions

--- Q 43-45/Q 43-45.js
+++ Q 43-45/Q 43-45.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 console.log(`***************************Day-15*********************************\n\n`);
 //Question 41: Magicians: Display magician names from an array.
 // Explain & TIP: Passing arrays to functions allows for the manipulation or display of their contents in a centralized manner. This is useful for handling lists of data.
