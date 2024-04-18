611afc0d6a9905dcfb3a93059ea866caa7b85dcc
Tuba Jan <121048565+Tuba282@users.noreply.github.com>
2024-04-18 14:22:52 +0500

    Update Q 7-9.ts

--- Q 7-9/Q 7-9.ts
+++ Q 7-9/Q 7-9.ts
@@ -5,37 +5,36 @@ console.log('Question 7: Number Eight: Write addition, subtraction, multiplicati
 //***********************************Q7 -Q8***************************************** */
 
 //Addition
-let num_1:number=28;
-let num_2:number=2;
+let num_1:number=2;
+let num_2:number=6;
 let num_3:number=num_1+num_2;
 console.log(num_1+num_2);
-console.log(num_3);
+console.log(num_3);//8
 
 //Subtraction
-let no1:number=2;
-let no2:number=3;
+let no1:number=13;
+let no2:number=5;
 let no3:number=no2-no1;
 console.log(no3-no2);
-console.log(no3);
+console.log(no3);//8
 
 //Multiplication
-let mul1:number=5;
+let mul1:number=2;
 let mul2:number=4;
-let mul3:number=3;
-console.log(mul1*mul2);
-console.log(mul3);
+console.log(mul1*mul2);//8
+
 
 
 //Division
-let div1:number=28;
+let div1:number=18;
 let div2:number=2;
 let div3:number=div1 /div2;
-console.log(div1/div2);
-console.log(div3);
+console.log(div1/div2);//8
+console.log(div3);//8
 
 //********************************************************Q9********************************* */
 
-// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+// Question 9: Favorite Number: Store your favourite number in a variable. Then, create a message revealing your favorite number. Print that message.
 
 
 let fav_num:number=7;
